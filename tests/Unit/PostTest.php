b1f113d6be55934fa6507248b2647cf76c7abbd7
Michael Burton <me@mikeylicio.us>
2018-08-25 17:37:26 +0100

    Make PHPUnit Output Prettier

---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostTest extends TestCase
+{
+    /** @test */
+    public function it_has_a_title()
+    {
+        //   
+    }
+
+    // /** @test */
+    // public function it_has_a_body()
+    // {
+    //     //
+    // }
+
+    // /** @test */
+    // public function it_has_an_author()
+    // {
+    //     //
+    // }
+
+    // /** @test */
+    // public function it_has_an_associated_image()
+    // {
+    //     //
+    // }
+}
