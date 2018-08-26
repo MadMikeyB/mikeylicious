f24d7aa8be1ce04d3d864ab778d9b0016ebf9ebe
Michael Burton <me@mikeylicio.us>
2018-08-26 20:07:05 +0100

    Update wording

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,8 +8,8 @@
 
         @slot('description')
             <p>I'm a {{ date('Y') - 1991 }} year old web developer from Nottingham in the UK.</p> 
-            <p>I specialise in <a href="https://laravel.com" target="_blank">Laravel</a>, but I am also proficient in <a href="#languages" class="scrolly">many other programming languages</a>.</p> 
-            <p>I have been developing for the web for {{ date('Y') - 2004 }} years, and have been doing so commercially for {{ date('Y') - 2010 }} years.</p>
+            <p>I specialise in <a href="https://laravel.com" target="_blank">Laravel</a>, and I am also proficient in <a href="#languages" class="scrolly">many other programming languages</a>.</p> 
+            <p>I have been using web technologies for {{ date('Y') - 2004 }} years, {{ date('Y') - 2010 }} of those commercial.</p>
             <p>I'm currently the Lead Developer at <a href="https://fifteen.co.uk" target="_blank">Fifteen</a>, and <a href="#contact" class="scrolly">available for freelance work.</a></p>
             <br>
         @endslot
